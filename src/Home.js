8e24a6e3bc5ff19ba4995ed9cd525780cc2f737a
qubitsecure <51173550+qubitsecure@users.noreply.github.com>
2025-02-14 12:54:19 +0000

    layout2

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import './index.css';
 import './Sidebar.css';
+import jenniferg from './assets/jenniferg.jpg';
 
 const Home = () => {
     return (
@@ -13,15 +14,22 @@ const Home = () => {
             <div className='masthead'> Maddy Swan </div>
             <p className='subs'> FINE ART </p>
             </div> 
+
+            <div class='flexbox'>
+            <div className='mainpicture_img'>
+            <img src={jenniferg} alt='Jennifer G' />
+              </div> 
+                  </div>
   
       <div className='maincontent'>
+          <div className='maincontent_text'>
             <p> Welcome to my website. Here you can view my work, find out more about me and contact me. </p>
-              <div className='mainpicture'>
-              <img src='./jenniferg.jpg' alt='Jennifer G' />
-              </div> 
-
+            <p> I am a fine artist based in London. I work with a variety of mediums including oil, acrylic, watercolour and pencil. I am inspired by nature, the human form and the world around me. </p>
+            <p> I have exhibited my work in galleries across the UK and have sold my work to private collectors. </p>  
+            
               </div>   
         
+      </div>
       </div>
 
       );
