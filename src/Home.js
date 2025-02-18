4fcc1e4254d5c5be42d3fe42918a75ab5f376ee3
qubitsecure <51173550+qubitsecure@users.noreply.github.com>
2025-02-18 22:47:32 +0000

    final version before new branch

--- src/Home.js
+++ src/Home.js
@@ -17,14 +17,18 @@ const Home = () => {
   
       <div className='maincontent'>
           <div className='maincontent_text'>        
-            <p> Welcome to my website. Here you can view my work, find out more about me and contact me. </p>
-            <p> I am a fine artist based in England. I work with a variety of mediums including oil, acrylic, watercolour and pencil. I am inspired by nature, the human form and the world around me. </p>
-            <p> I have exhibited my work in galleries across the UK and have sold my work to private collectors. </p>  
-            </div>
-           
+          <p>Primarily I am a portrait artist who specialises in adults, teens, children and occasionally pets. I also produce landscapes, figure studies and botanical sketches.
+</p>
+<p>The media I use is watercolour, oil, acrylic, pastel, charcoal, colour pencil and graphite. In essence I like experimenting with any medium and material that inspires me.
+</p>
+<p>Watercolour is my favourite medium right now; it's versatile and challenging, with lovely qualities of texture and colour. I have also been using high standard artist quality coloured pencils for a number of years to execute portraits and other subjects: this medium gives a refined quality and allows for lots of detail. However, watercolour allows more freedom of expression, especially for portraits. It's also a wonderful medium for landscapes, buildings, water scenes etc. Apart from these I have taken up oil painting recently and I have produced a number of portraits, some of which are featured on my YouTube channel.
+<p>I continue to develop skills essential for my portraits as well as being involved is experimenting with landscape technique; with some results more successful than others.
+  </p>
+      </p>     
       </div>
    
-    </div>
+      </div>
+      </div>
     
 
       );
