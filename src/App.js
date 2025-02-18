f8cd9dc447037f1db16a9ad5163f42d56a411dc2
qubitsecure <51173550+qubitsecure@users.noreply.github.com>
2025-02-18 19:19:45 +0000

    corrections

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import RootLayout from './layouts/RootLayout';
 import Home from './Home';
 import Portraits from './layouts/Portraits';
 import Footer from './Footer';
+import './index.css';
 
 
 
@@ -11,8 +13,8 @@ const router = createBrowserRouter(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} /> {/* This sets Home as the default route */}
       <Route path="/Home" element={<Home />} />
-      <Route path="/Footer" element={<Footer />} />
       <Route path="/Portraits" element={<Portraits />} />
+      <Route path="/footer" element={<Footer />} />
 
       </Route>
   )
