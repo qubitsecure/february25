14cacaed28f94e1eaac070361e869b4c39142718
qubitsecure <51173550+qubitsecure@users.noreply.github.com>
2025-02-19 15:59:15 +0000

    ``` refactor code structure for improved readability ```last

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import './index.css';
 import './Sidebar.css';
 import jenniferg from './assets/jenniferg.jpg';
+import ms_36 from './assets/ms_36.jpg';
 
 const Home = () => {
     return (
@@ -21,6 +22,11 @@ const Home = () => {
 </p>
 <p>The media I use is watercolour, oil, acrylic, pastel, charcoal, colour pencil and graphite. In essence I like experimenting with any medium and material that inspires me.
 </p>
+<div id="container">
+<div className='secondary_img'>
+            <img src={ms_36.jpg} alt='ms_36' />
+              </div> 
+              </div>
 <p>Watercolour is my favourite medium right now; it's versatile and challenging, with lovely qualities of texture and colour. I have also been using high standard artist quality coloured pencils for a number of years to execute portraits and other subjects: this medium gives a refined quality and allows for lots of detail. However, watercolour allows more freedom of expression, especially for portraits. It's also a wonderful medium for landscapes, buildings, water scenes etc. Apart from these I have taken up oil painting recently and I have produced a number of portraits, some of which are featured on my YouTube channel.
 <p>I continue to develop skills essential for my portraits as well as being involved is experimenting with landscape technique; with some results more successful than others.
   </p>
