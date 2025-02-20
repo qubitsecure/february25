3a5301f9db47f5ea122ffbe122ff824ed5fddd74
qubitsecure <51173550+qubitsecure@users.noreply.github.com>
2025-02-20 22:20:50 +0000

    gallery page added

--- src/layouts/Exhibitions.js
+++ src/layouts/Exhibitions.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import ms_44 from '../assets/ms_44.jpg';
+import ms_33 from '../assets/ms_33.jpg';
 
 
 const Exhibitions = () => {
     return (
-        <div className ='headline2'>
+
+      <div className='main-container'>
+       <div className="maincontent2">
+       </div>
+      <div className ='headline2'>
             <p>Exhibitions</p>
-            {/* Add your portrait components here */}
-        </div>
-    );
-};
+            </div>
+       <div className="maincontent">    
+          <div className='maincontent_text'>        
+          <p>Primarily I am a portrait artist who specialises in adults, teens, children and occasionally pets. I also produce landscapes, figure studies and botanical sketches. The media I use is watercolour, oil, acrylic, pastel, charcoal, colour pencil and graphite. In essence I like experimenting with any medium and material that inspires me.
+</p>
+<p>Watercolour is my favourite medium right now; it's versatile and challenging, with lovely qualities of texture and colour. I have also been using high standard artist quality coloured pencils for a number of years to execute portraits and other subjects: this medium gives a refined quality and allows for lots of detail. However, watercolour allows more freedom of expression, especially for portraits. It's also a wonderful medium for landscapes, buildings, water scenes etc. Apart from these I have taken up oil painting recently and I have produced a number of portraits, some of which are featured on my YouTube channel.
+<p>I continue to develop skills essential for my portraits as well as being involved is experimenting with landscape technique; with some results more successful than others.
+  </p> 
+      </p> 
+      </div>
+<div className='tertiary_img'>
+<img src={ms_44} alt='ms_44' />
+</div>
+
+      <div className='secondary_img'>
+            <img src={ms_33} alt='ms_33' />
+              </div> 
+              <div className='maincontent_text'>        
+          <p>Primarily I am a portrait artist who specialises in adults, teens, children and occasionally pets. I also produce landscapes, figure studies and botanical sketches. The media I use is watercolour, oil, acrylic, pastel, charcoal, colour pencil and graphite. In essence I like experimenting with any medium and material that inspires me.
+</p>
+<p>Watercolour is my favourite medium right now; it's versatile and challenging, with lovely qualities of texture and colour. I have also been using high standard artist quality coloured pencils for a number of years to execute portraits and other subjects: this medium gives a refined quality and allows for lots of detail. However, watercolour allows more freedom of expression, especially for portraits. It's also a wonderful medium for landscapes, buildings, water scenes etc. Apart from these I have taken up oil painting recently and I have produced a number of portraits, some of which are featured on my YouTube channel.
+<p>I continue to develop skills essential for my portraits as well as being involved is experimenting with landscape technique; with some results more successful than others.
+  </p> 
+      </p> 
+      </div>    
+      </div>
+      </div>
+
+      );
+}
 
 export default Exhibitions;
