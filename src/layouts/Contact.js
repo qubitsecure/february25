15ae899a1b37ee6a6c7fc1d9684cad8acf58db80
qubitsecure <b90003@yahoo.com>
2025-08-31 22:03:01 +0100

    new aug

--- src/layouts/Contact.js
+++ src/layouts/Contact.js
@@ -1,26 +1,45 @@
-import { Form } from 'react-router-dom';
 import '../index.css';
+import op4 from '../assets/op4.jpg';
+
 
 
 export default function Contact() {
-    return (
-        <div className= "contact">
-            <div className ='headline2'>
-            <p>Contact</p>
-        </div>
 
-        <Form method="post" action="">
-            <label>
-                <span> Your email:</span>
-                <input type="email" name="email" required/>
-            </label>
-            <label>
-                <span> Your message:</span>
-                <textarea name="message" required></textarea>
-            </label>
-            <button>Submit</button>
-        </Form>
+
+    return (
+
+         <div className='container'>            
+            <div className="headline2">
+                <p>Contact</p>
+            </div>
+             
+         <div className='contact-container'>
+            <div className="contact-form">
+                <form method="post" action="" className="custom-contact-form">
+                    <div className="form-group">
+                        <label htmlFor="email">
+                            <span>Your email</span>
+                        </label>
+                        <input type="email" id="email" name="email" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message">
+                            <span>Your message</span>
+                        </label>
+                        <textarea id="message" name="message" required></textarea>
+                    </div>
+                    <div className="form-buttons">
+                        <button type="submit" className="custom-submit-btn">Send</button>
+                        <button type="reset" className="custom-reset-btn">Clear</button>
+                    </div>
+                    <div className='secondary_img'>
+                    <img src={op4} alt='op4' />
+          </div>
+                </form>
+            </div>
+
         </div>
+        </div>
+        
     );
 }
-
